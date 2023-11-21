f774c2e47710b4a05254b5c29efa7043316d5dbc
Diana <dianamingazova162@gmail.com>
2023-11-22 02:07:53 +0500

    Выполнила ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        Bicycle bicycle = new Bicycle("Forward");
+        Car bmw = new Car("BMW M5");
+        Car camry = new Car("Toyota Camry");
+        Truck truck = new Truck("Камаз");
+        Truck truck2 = new Truck("Маз", 8);
 
-        System.out.println("Hello world!");
+        Transport[] transports = {
+                bicycle,
+                bmw,
+                camry,
+                truck,
+                truck2,
+        };
+
+        ServiceStation serviceStation = new ServiceStation();
+
+        for (Transport transport : transports) {
+            serviceStation.check(transport);
+            System.out.println("");
+        }
     }
 }
